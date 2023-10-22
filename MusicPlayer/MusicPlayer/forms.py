13544f5b65de8586e8d5cc2a10436fb7f1bc1116
Migas77 <miguel.belchior@ua.pt>
2023-10-22 14:17:46 +0100

    Login

---
+++ MusicPlayer/MusicPlayer/forms.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class LoginForm(AuthenticationForm):
+    # AuthenticationForm -> Default Login Form
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        '''
+        self.username.widget_attrs({
+            'type': 'email',
+            'class': 'form-control',
+            'id': 'InputUsername',
+            'aria-describedby': 'usernameHelp'
+        })
+        '''
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'id': 'InputUsername1',
+            'placeholder': 'Username',
+            'aria-describedby': 'usernameHelp',
+        })
+        self.fields["password"].widget.attrs.update({
+            'type': 'password',
+            'class': 'form-control',
+            'placeholder': 'Password',
+            'id': 'InputPassword1'
+        })
+
--- MusicPlayer/MusicPlayer/urls.py
+++ MusicPlayer/MusicPlayer/urls.py
@@ -15,12 +15,18 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
+from MusicPlayer.forms import *
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # login / logout
+    path('login/', auth_views.LoginView.as_view(template_name='login.html', authentication_form=LoginForm, next_page='home'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    # others
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
