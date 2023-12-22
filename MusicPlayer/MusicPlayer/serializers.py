12c12b6ba5658a8fa915dc7a9f54bcd488b410b8
Migas77 <miguel.belchior@ua.pt>
2023-12-22 08:39:10 +0000

    Fix: hashing passwords now

--- MusicPlayer/MusicPlayer/serializers.py
+++ MusicPlayer/MusicPlayer/serializers.py
@@ -4,6 +4,13 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
     class Meta:
         model = Listener
         fields = ('id', 'email', 'username', 'password')
